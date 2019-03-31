dad42f613ef6169d480e407c6e06bca9bd7fe134
Zalron <Davidrlawson@optusnet.com.au>
2019-03-31 17:04:03 +1100

    Added unity project

---
+++ BlockClasses/Assets/DirtBlock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtBlock : Block {
+
+	public Vector2[,] myUVs = { 
+		/*TOP*/			{new Vector2( 0.125f, 0.9375f ), new Vector2( 0.1875f, 0.9375f),
+								new Vector2( 0.125f, 1.0f ),new Vector2( 0.1875f, 1.0f )},
+		/*SIDE*/		{new Vector2( 0.125f, 0.9375f ), new Vector2( 0.1875f, 0.9375f),
+								new Vector2( 0.125f, 1.0f ),new Vector2( 0.1875f, 1.0f )},
+		/*BOTTOM*/		{new Vector2( 0.125f, 0.9375f ), new Vector2( 0.1875f, 0.9375f),
+								new Vector2( 0.125f, 1.0f ),new Vector2( 0.1875f, 1.0f )}};
+
+	public DirtBlock(Vector3 pos, GameObject p, Material c)
+	{
+		bType = BlockType.DIRT;
+		parent = p;
+		position = pos;
+		cubeMaterial = c;
+		isSolid = true;
+
+		blockUVs = myUVs;
+	}
+
+}
